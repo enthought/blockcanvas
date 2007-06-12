d2ea634a1d16d0fb460dfb219d73ebe8eb4e4d7f
bryanv <bryanv@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-06-12 21:36:45 +0000

    unit traits support default values and units

--- enthought/numerical_modeling/units/traits/unit_array_trait.py
+++ enthought/numerical_modeling/units/traits/unit_array_trait.py
@@ -7,9 +7,18 @@
 from enthought.numerical_modeling.units.api import UnitArray as UA
 from enthought.numerical_modeling.units.traits.ui.unit_editor \
     import UnitEditor
-from enthought.traits.api import Instance
-from enthought.units.unit import dimensionless 
+from enthought.traits.api import Instance, TraitFactory
 
 # Local Imports
 
-UnitArrayTrait = Instance(UA, editor=UnitEditor)
+def UnitArrayTrait(value=None, units=None, **kwargs):
+    args = ()
+    kw = {}
+    if value and units:
+        args = (value,)
+        kwargs['args'] = args
+        kw = {'units':units}
+        kwargs['kw'] = kw
+    return Instance(UA, editor=UnitEditor, **kwargs)
+
+UnitArrayTrait = TraitFactory(UnitArrayTrait)
