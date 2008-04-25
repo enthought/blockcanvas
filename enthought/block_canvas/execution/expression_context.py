0f1153094582f8bd0fe2b46f4d48c2849c5ff287
kammeyer <kammeyer@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-04-25 18:48:55 +0000

    Mostly working version of ExpressionContext

--- enthought/block_canvas/execution/expression_context.py
+++ enthought/block_canvas/execution/expression_context.py
@@ -1,7 +1,14 @@
-from enthought.traits.api import implements
+# Global imports
 from UserDict import DictMixin
-from data_context import ListenableMixin, PersistableMixin
-from i_context import ICheckpointable, IListenableContext, IPersistableContext
+
+# Enthought library imports
+from enthought.traits.api import implements, Str, Dict, Any, List, Instance, on_trait_change
+
+# Block Canvas imports
+from enthought.numerical_modeling.workflow.block.api import Block
+from enthought.block_canvas.context.data_context import ListenableMixin, PersistableMixin, DataContext
+from enthought.block_canvas.context.i_context import IContext, IListenableContext, ICheckpointable, \
+                                                     IPersistableContext
 
 class ExpressionContext(ListenableMixin, PersistableMixin, DictMixin):
     """Provide a context wrapper that adds the ability to request expressions on variables
@@ -19,14 +26,15 @@ class ExpressionContext(ListenableMixin, PersistableMixin, DictMixin):
     # The currently evaluated expressions, mapped to their last cached value.  None means
     # that the value is not cached.
     _expressions = Dict(Str, Any)
-    # A list of 
+    # A list of variable dependencies for each expression
     _dependencies = Dict(Str, List(Str))
 
-    dependencies = Dict(Str, List(Str))
     def __init__(self, underlying_context, **traits):
         super(ExpressionContext, self).__init__(underlying_context=underlying_context)
 
     def __delitem__(self, key):
+        """Delete an item from the ExpressionContext -- either in the underlying context,
+        or if an already cached expression, delete it."""
         # if item is an expression, delete it from the list of dependencies, otherwise pass it down
         # to underlying
         if key in self._expressions:
@@ -38,13 +46,31 @@ class ExpressionContext(ListenableMixin, PersistableMixin, DictMixin):
         # FIXME: not sure what to do here -- maybe eval the expression in the context
         # and see if it works as an expression on this context if it's not in the underly
         # -ing
-
+        return key in self.underlying_context
+    
     def __getitem__(self, key):
         if key in self.underlying_context.keys():
             return self.underlying_context[key]
         else:
-            #eval expression if not cached and return
-            pass
+            try:
+                # FIXME imports need to be more configurable
+                # FIXME we may want to have cache rules on sizes so that we
+                # don't keep around huge values that aren't needed anymore
+                # however, there is a time/space tradeoff that really should be
+                # used.  We currently support del so that is available if the user
+                # wants to manually manage this.
+                eval_globals = {}
+                for lib in (__builtins__, __import__('numpy')):
+                    for sym in dir(lib):
+                        if not key.startswith('__'):
+                            eval_globals[sym] = getattr(lib, sym)
+
+                result = eval(key, eval_globals, self.underlying_context)
+                self._expressions[key] = result
+                self._dependencies[key] = list(Block(key).inputs)
+                return result
+            except:
+                return None
 
     def __setitem__(self, key, value):
         """We don't allow setting an expression per se, so it passes through
@@ -72,7 +98,8 @@ class ExpressionContext(ListenableMixin, PersistableMixin, DictMixin):
                             event_list.append(dep_item)
                         # Remove the cached value for the item
                         self._expressions[dep_item] = None
-            return
+        self.items_modified = event
+        return
 
     def _underlying_context_changed(self):
         self._expressions = {}
