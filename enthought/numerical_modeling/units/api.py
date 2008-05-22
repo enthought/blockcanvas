ee144d13625f7444194b89b4863fad0d65179097
oliphant <oliphant@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-05-22 21:30:18 +0000

    Export function inspection pieces out of has_units code.  These functions should be re-factored into a set of code-analysis tools for ETS.

--- enthought/numerical_modeling/units/api.py
+++ enthought/numerical_modeling/units/api.py
@@ -1,3 +1,5 @@
 from has_units import has_units
+from function_signature import (call_signature, def_signature,
+                                function_arguments)
 from unit_array import UnitArray
 from unit_scalar import UnitScalar
