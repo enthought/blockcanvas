16d8b3369967e2d20168de1e77c1833b035226fd
oliphant <oliphant@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-06-26 16:22:48 +0000

    Remove enthought.numerical_modeling.workflow.block entirely to avoid confusion.

--- enthought/numerical_modeling/workflow/block/api.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-warnings.warn("""This location is deprecated.  Please update your source file:  enthought.numerical_modeling.workflow.block -> enthought.blocks""", DeprecationWarning)
-
-from analysis import NameFinder, free_vars, local_vars, conditional_local_vars
-from block import Block, Expression, to_block
-from compiler_.api import compile_ast, parse
-from compiler_unparse import unparse
-from rename import rename
