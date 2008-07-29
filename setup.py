86fa0338520ffc168efceed708e0022790fe2c3e
swisher <swisher@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-29 21:10:07 +0000

    Added long description in docstring.

--- setup.py
+++ setup.py
@@ -7,7 +7,20 @@
 """
 Numerical Modeling
 
-<description text needed>
+The BlockCanvas project provides a visual environment for creating simulation
+experiments, where function and data are separated. Thus, you can define your
+simulation algorithm by visually connecting function blocks into a data flow
+network, and then run it with various data sets (known as "contexts"); 
+likewise, you can use the same context in a different functional simulation.
+
+The project provides support for plotting, function searching and inspection,
+and optimization. It includes a stand-alone application that demonstrates the
+block-canvas environment, but the same functionality can be incorporated into
+other applications.
+
+The BlockCanvas project relies on included libraries that allow multiple data
+sets using Numeric arrays to be incorporated in a Traits-based model in a 
+way that is simple, fast, efficient, and consistent. 
 """
 
 
