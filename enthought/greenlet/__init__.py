3aa44a844ea8615f2d0c18d17ddcaac296bfcd07
swisher <swisher@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-11 17:17:34 +0000

    Added the project to the packages' docstrings. This is obvious when looking at code, but not when looking at API docs.

--- enthought/greenlet/__init__.py
+++ enthought/greenlet/__init__.py
@@ -1,1 +1,9 @@
+#-----------------------------------------------------------------------------
+#
+#  Copyright (c) 2007 by Enthought, Inc.
+#  All rights reserved.
+#
+#-----------------------------------------------------------------------------
+""" Part of the BlockCanvas project of the Enthought Tool Suite.
+"""
 from greenlet import *
--- enthought/model/__init__.py
+++ enthought/model/__init__.py
@@ -1,7 +1,5 @@
 #-------------------------------------------------------------------------------
 #  
-#  Package for reusable data models.  
-#  
 #  Written by: David C. Morrill
 #  
 #  Date: 09/30/2005
@@ -9,3 +7,6 @@
 #  (c) Copyright 2005 by Enthought, Inc.
 #  
 #-------------------------------------------------------------------------------
+""" Package for reusable data models. 
+    Part of the BlockCanvas project of the Enthought Tool Suite.
+"""
