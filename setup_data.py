a0b7880bbd5ffb1cbb041f54410dea216dcec3ee
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-10-13 16:20:30 +0000

    * Updated to depend on new version of AppTools.

--- setup_data.py
+++ setup_data.py
@@ -14,7 +14,7 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies.
-APPTOOLS = etsdep('AppTools', '3.0.1')  # -- all from enthought.block_canvas' use of enthought.undo
+APPTOOLS = etsdep('AppTools', '3.1.0')  # -- all from enthought.block_canvas' use of enthought.undo
 CHACO = etsdep('Chaco', '3.0.1')
 ENABLE_TRAITS = etsdep('Enable[traits]', '3.0.2')  # -- all from enthought.block_canvas' use of enthought.kiva.traits
 ENTHOUGHTBASE_DISTRIBUTION_UI = etsdep('EnthoughtBase[distribution,ui]', '3.0.1')
