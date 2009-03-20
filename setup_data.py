a81708b8cd7f112f92e98d42e5f6fe7fe915d411
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-20 18:01:22 +0000

    * Updated dependency versions to match current trunk versions (some checkins may not have been made yet for other projects though.  Just give me a second.)

--- setup_data.py
+++ setup_data.py
@@ -14,14 +14,14 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies.
-APPTOOLS = etsdep('AppTools', '3.1.1')  # -- all from enthought.block_canvas' use of enthought.undo
-CHACO = etsdep('Chaco', '3.0.2')
-ENABLE_TRAITS = etsdep('Enable[traits]', '3.0.3')  # -- all from enthought.block_canvas' use of enthought.kiva.traits
+APPTOOLS = etsdep('AppTools', '3.2.0')  # -- all from enthought.block_canvas' use of enthought.undo
+CHACO = etsdep('Chaco', '3.1.0')
+ENABLE_TRAITS = etsdep('Enable[traits]', '3.1.0')  # -- all from enthought.block_canvas' use of enthought.kiva.traits
 ENTHOUGHTBASE_DISTRIBUTION_UI = etsdep('EnthoughtBase[distribution,ui]', '3.0.2')
 ETSDEVTOOLS = etsdep('ETSDevTools', '3.0.2')  # -- all from enthought.block_canvas' use of enthought.testing.api
 SCIMATH_TRAITS = etsdep('SciMath[traits]', '3.0.3')
-TRAITS_UI = etsdep('Traits[ui]', '3.0.4')
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.4')
+TRAITS_UI = etsdep('Traits[ui]', '3.1.0')
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.1.0')
 TRAITSGUI = etsdep('TraitsGUI', '3.0.4')
 
 
