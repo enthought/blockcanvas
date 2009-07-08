1eefc34492af640a7220695462e39e33bc6c7146
GaelVaroquaux <GaelVaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-08 01:57:09 +0000

    ENH: Capture any errors in setupdocs and fail gracefully during the build.

--- setup.py
+++ setup.py
@@ -29,9 +29,8 @@ If you want to build BlockCanvas from source, you must first install
 
 """
 
-
-import os
-import zipfile
+import traceback
+import sys
 
 from distutils import log
 from distutils.command.build import build as distbuild
@@ -87,12 +86,21 @@ greenlet = Extension(
 class MyDevelop(develop):
     def run(self):
         develop.run(self)
-        self.run_command('build_docs')
+        try:
+            self.run_command('build_docs')
+        except:
+            log.warn("Couldn't build documentation:\n%s" %
+                     traceback.format_exception(*sys.exc_info()))
+
 
 class MyBuild(distbuild):
     def run(self):
         distbuild.run(self)
-        self.run_command('build_docs')
+        try:
+            self.run_command('build_docs')
+        except:
+            log.warn("Couldn't build documentation:\n%s" %
+                     traceback.format_exception(*sys.exc_info()))
 
 
 # The actual setup call.
