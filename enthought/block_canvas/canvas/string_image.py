aaa456d29ed62a272f9d0d22be340bff3b395763
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2011-01-12 00:06:00 +0000

    incorporating enthought.enable's deprecated gc_image_for() function into string_image.py in BlockCanvas, since this is the only remaining place where it is still used

--- enthought/block_canvas/canvas/string_image.py
+++ enthought/block_canvas/canvas/string_image.py
@@ -1,7 +1,54 @@
 from enthought.traits.api import Str
 from enthought.pyface.image_resource import ImageResource
 
-from enthought.enable.base import gc_image_for
+import sys
+from zipfile import ZipFile, is_zipfile
+from cStringIO import StringIO
+from os.path import abspath
+from types import TupleType
+from enthought.kiva.backend_image import Image
+
+# The gc_image_for() function was formerly in Enable but was deprecated,
+# and since the StringImage class is the only place that used it, it has
+# been moved here.  The parts of it that referenced the images/ directory
+# in enthought.enable have been removed.
+
+# Image cache dictionary (indexed by 'normalized' filename):
+_image_cache = {}
+_zip_cache   = {}
+_app_path    = None
+_enable_path = None
+
+def gc_image_for ( name, path = None ):
+    "Convert an image file name to a cached Kiva gc containing the image"
+    global _app_path, _enable_path
+    filename = abspath( name )
+    image     = _image_cache.get( filename )
+    if image is None:
+        cachename = filename
+        if path is not None:
+            zip_path = abspath( path + '.zip' )
+            zip_file = _zip_cache.get( zip_path )
+            if zip_file is None:
+               if is_zipfile( zip_path ):
+                   zip_file = ZipFile( zip_path, 'r' )
+               else:
+                   zip_file = False
+               _zip_cache[ zip_path ] = zip_file
+            if isinstance( zip_file, ZipFile ):
+                try:
+                    filename = StringIO( zip_file.read( name ) )
+                except:
+                    pass
+        try:
+            _image_cache[ cachename ] = image = Image( filename )
+        except:
+            _image_cache[ filename ] = info = sys.exc_info()[:2]
+            raise info[0], info[1]
+    elif type( image ) is TupleType:
+        raise image[0], image[1]
+    return image
+
 
 class StringImage(Str):
 
